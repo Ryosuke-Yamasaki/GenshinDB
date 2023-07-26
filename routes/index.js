01080d73a6323378c9a3266974b7ac01a81d2840
Ryosuke-Yamasaki <s2032144bu@s.chibakoudai.jp>
2023-07-26 23:51:08 +0900

    npm install mysql

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
 
-/* GET home page. */
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'TBdEAm3jKW&a',
+  database: 'genshin_db'
+});
+
+//ejsに渡すやつ
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Genshin DB' });
+});
+
+//ejsから受け取ったやつを処理するやつ
+router.post('/', function(req, res, next) {
+  res.render('index', { title: 'Genshin DB' });
 });
 
 module.exports = router;
