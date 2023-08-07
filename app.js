0734090a1a911e3a239eca47232362efe6cc5c98
Ryosuke-Yamasaki <s2032144bu@s.chibakoudai.jp>
2023-08-07 13:57:27 +0900

    片手剣の画像追加

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static('/images'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // authorization
 require("./config/passport")(app);
@@ -27,12 +26,12 @@ require("./config/passport")(app);
 app.use('/', require('./routes'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
