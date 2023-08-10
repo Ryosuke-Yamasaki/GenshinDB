9ea76fa74057da4aba7d1198375c7a9a1d5455c6
Ryosuke-Yamasaki <s2032144bu@s.chibakoudai.jp>
2023-08-10 12:59:54 +0900

    固有天賦を表示させた

--- routes/index.js
+++ routes/index.js
@@ -7,23 +7,82 @@ router.get('/', function (req, res) {
   const isAuth = req.isAuthenticated();
   if (isAuth) {
     const characterId = 31;
-    const level = "level_90";
+    const characterlevel = "level_90";
+    const talentsnormal = "level_8";
+    const talentselementalskill = "level_8";
+    const talentselementalburst = "level_8";
     knex("character")
       .select("*")
       .where({ id: characterId })
       .then(function (results1) {
         console.log(results1);
         knex("character_status")
-          .select("status", level)
+          .select("status", characterlevel)
           .where({ character_id: characterId })
           .then(function (results2) {
             console.log(results2);
-            res.render('index', {
-              title: 'Genshin DB',
-              characters: results1,
-              levels: results2,
-              isAuth: isAuth,
-            });
+            knex("talents")
+              .select("talents_type", "name", talentsnormal)
+              .where({ character_id: characterId, talents_type: 1 })
+              .then(function (results3) {
+                console.log(results3);
+                knex("talents")
+                  .select("talents_type", "name", talentselementalskill)
+                  .where({ character_id: characterId, talents_type: 2 })
+                  .then(function (results4) {
+                    console.log(results4);
+                    knex("talents")
+                      .select("talents_type", "name", talentselementalburst)
+                      .where({ character_id: characterId, talents_type: 3 })
+                      .then(function (results5) {
+                        console.log(results5);
+                        knex("passive")
+                          .select("name", "effect")
+                          .where({ character_id: characterId })
+                          .then(function (results6) {
+                            console.log(results6);
+                            res.render('index', {
+                              title: 'Genshin DB',
+                              characters: results1,
+                              levels: results2,
+                              talents_normal: results3,
+                              talents_elementalslill: results4,
+                              talents_elementalburst: results5,
+                              passives: results6,
+                              isAuth: isAuth,
+                            });
+                          })
+                          .catch(function (err) {
+                            console.log(err);
+                            res.render('index', {
+                              title: 'Genshin DB',
+                              isAuth: isAuth,
+                            });
+                          });
+                      })
+                      .catch(function (err) {
+                        console.log(err);
+                        res.render('index', {
+                          title: 'Genshin DB',
+                          isAuth: isAuth,
+                        });
+                      });
+                  })
+                  .catch(function (err) {
+                    console.log(err);
+                    res.render('index', {
+                      title: 'Genshin DB',
+                      isAuth: isAuth,
+                    });
+                  });
+              })
+              .catch(function (err) {
+                console.log(err);
+                res.render('index', {
+                  title: 'Genshin DB',
+                  isAuth: isAuth,
+                });
+              });
           })
           .catch(function (err) {
             console.log(err);
