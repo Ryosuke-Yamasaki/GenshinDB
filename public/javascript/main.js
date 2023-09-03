9a1ce7985c388306581aa6f362bd8300fa992612
Ryosuke-Yamasaki <s2032144bu@s.chibakoudai.jp>
2023-09-04 01:49:25 +0900

    各攻撃ごとの倍率を出力できるようにした

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -7,3 +7,9 @@ const totalhp = basehp + bonushp;
 ele_basehp.textContent = basehp;
 ele_bonushp.textContent = bonushp;
 ele_totalhp.textContent = totalhp;
+
+const ele_a = document.querySelectorAll(".normal");
+for (let i = 0; i < ele_a.length; i++) {
+  const a = parseFloat(ele_a[i].textContent);
+  console.log(a + 1);
+}
