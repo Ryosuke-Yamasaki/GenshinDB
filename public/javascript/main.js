326e02a381d2ff36a164bb592e9fdf2d5eb70302
Ryosuke-Yamasaki <s2032144bu@s.chibakoudai.jp>
2023-09-13 17:06:56 +0900

    クラスとかの追加

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,13 +1,97 @@
 //HP
-const ele_basehp = document.querySelector(".basehp");
-const ele_bonushp = document.querySelector(".bonushp");
-const ele_totalhp = document.querySelector(".totalhp");
-const basehp = parseFloat(ele_basehp.textContent);
-const bonushp = parseFloat(ele_bonushp.textContent);
-const totalhp = basehp + bonushp;
-ele_basehp.textContent = basehp;
-ele_bonushp.textContent = bonushp;
-ele_totalhp.textContent = totalhp;
+const ele_baseHP = document.querySelector(".base-hp");
+const ele_bonusHP = document.querySelector(".bonus-hp");
+const ele_totalHP = document.querySelector(".total-hp");
+const baseHP = parseFloat(ele_baseHP.textContent);
+const bonusHP = parseFloat(ele_bonusHP.textContent);
+const totalHP = baseHP + bonusHP;
+ele_baseHP.textContent = baseHP;
+ele_bonusHP.textContent = bonusHP;
+ele_totalHP.textContent = totalHP;
+
+
+//ATK
+const ele_baseATK = document.querySelector(".base-atk");
+const ele_bonusATK = document.querySelector(".bonus-atk");
+const ele_totalATK = document.querySelector(".total-atk");
+const baseATK = parseFloat(ele_baseATK.textContent);
+const bonusATK = parseFloat(ele_bonusATK.textContent);
+const totalATK = baseATK + bonusATK;
+ele_baseATK.textContent = baseATK;
+ele_bonusATK.textContent = bonusATK;
+ele_totalATK.textContent = totalATK;
+
+
+//DEF
+const ele_baseDEF = document.querySelector(".base-def");
+const ele_bonusDEF = document.querySelector(".bonus-def");
+const ele_totalDEF = document.querySelector(".total-def");
+const baseDEF = parseFloat(ele_baseDEF.textContent);
+const bonusDEF = parseFloat(ele_bonusDEF.textContent);
+const totalDEF = baseDEF + bonusDEF;
+ele_baseDEF.textContent = baseDEF;
+ele_bonusDEF.textContent = bonusDEF;
+ele_totalDEF.textContent = totalDEF;
+
+
+//EM
+const ele_baseEM = document.querySelector(".base-em");
+const ele_bonusEM = document.querySelector(".bonus-em");
+const ele_totalEM = document.querySelector(".total-em");
+const baseEM = parseFloat(ele_baseEM.textContent);
+const bonusEM = parseFloat(ele_bonusEM.textContent);
+const totalEM = baseEM + bonusEM;
+ele_baseEM.textContent = baseEM;
+ele_bonusEM.textContent = bonusEM;
+ele_totalEM.textContent = totalEM;
+
+
+//ER
+const ele_baseER = document.querySelector(".base-er");
+const ele_bonusER = document.querySelector(".bonus-er");
+const ele_totalER = document.querySelector(".total-er");
+const baseER = parseFloat(ele_baseER.textContent);
+const bonusER = parseFloat(ele_bonusER.textContent);
+const totalER = baseER + bonusER;
+ele_baseER.textContent = baseER;
+ele_bonusER.textContent = bonusER;
+ele_totalER.textContent = totalER;
+
+
+//CR
+const ele_baseCR = document.querySelector(".base-cr");
+const ele_bonusCR = document.querySelector(".bonus-cr");
+const ele_totalCR = document.querySelector(".total-cr");
+const baseCR = parseFloat(ele_baseCR.textContent);
+const bonusCR = parseFloat(ele_bonusCR.textContent);
+const totalCR = baseCR + bonusCR;
+ele_baseCR.textContent = baseCR;
+ele_bonusCR.textContent = bonusCR;
+ele_totalCR.textContent = totalCR;
+
+
+//CD
+const ele_baseCD = document.querySelector(".base-cd");
+const ele_bonusCD = document.querySelector(".bonus-cd");
+const ele_totalCD = document.querySelector(".total-cd");
+const baseCD = parseFloat(ele_baseCD.textContent);
+const bonusCD = parseFloat(ele_bonusCD.textContent);
+const totalCD = baseCD + bonusCD;
+ele_baseCD.textContent = baseCD;
+ele_bonusCD.textContent = bonusCD;
+ele_totalCD.textContent = totalCD;
+
+
+//各ステータスの宣言
+const pyro_db = 0;
+const hydro_db = 0;
+const cryo_db = 0;
+const electro_db = 0;
+const anemo_db = 0;
+const geo_db = 0;
+const dendro_db = 0;
+const physical_db = 0;
+
 
 
 //攻撃倍率
@@ -66,6 +150,3 @@ function save_restore3_select(target_class) {
   }
 }
 save_restore3_select('save-state3');
-
-
-//
