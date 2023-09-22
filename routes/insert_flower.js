01a21c4c0ab5f7a6e4913ccf4d498b48cc74306b
Ryosuke-Yamasaki <s2032144bu@s.chibakoudai.jp>
2023-09-22 14:39:47 +0900

    data-substeの修正

--- routes/insert_flower.js
+++ routes/insert_flower.js
@@ -4,24 +4,36 @@ const knex = require('../db/knex');
 
 router.post('/', function (req, res) {
   const userId = req.user.id;
-  console.log(Object.values(req.body));
-  knex("artifact_myset")
-    .insert({
-      /*user_id: userId,
-      artifact_id: req.body[Object.keys],
-      parts_id: 1,
-      mainop_id: 1,
-      substatus1:,
-      substatus2:,
-      substatus3:,
-      substatus4:,
-      subnum1:,
-      subnum2:,
-      subnum3:,
-      subnum4:,*/
-    })
-    .then(function () {
-      res.redirect('/');
+  console.log(Object.keys(req.body));
+  knex("damage_myset")
+    .select("flowers_id")
+    .then(function (damage) {
+      knex("artifact_myset")
+        .insert({
+          user_id: userId,
+          artifact_id: damage[0]["flowers_id"],
+          parts_id: 1,
+          mainop_id: 1,
+          /*substatus1:req.body[],
+          substatus2:,
+          substatus3:,
+          substatus4:,
+          subnum1:,
+          subnum2:,
+          subnum3:,
+          subnum4:,*/
+        })
+        .then(function () {
+          res.redirect('/');
+        })
+        .catch(function (err) {
+          console.log(err);
+          res.render('index', {
+            title: 'Genshin DB',
+            errorMessage: [err.sqlMessage],
+            isAuth: isAuth,
+          });
+        })
     })
     .catch(function (err) {
       console.log(err);
